f57ef15be7ac690705bf7ab01627fee461f30272
Oshama Nmaberdar <oshamanamberdargujjar@gmail.com>
2021-06-17 02:05:40 +0545

    This is my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/src/audio_cache.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          //       child: Center(child: FlatButton(onPressed: (){
+          //         final player = AudioCache();
+          //         player.play('note1.wav');
+          //         },
+          // child: ,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // ignore: deprecated_member_use
+
+              // ignore: deprecated_member_use
+
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.blue, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.purple, soundNumber: 5),
+              buildKey(color: Colors.white, soundNumber: 6),
+              buildKey(color: Colors.grey, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
